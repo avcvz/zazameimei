971d380e976cc09e5f608490d0fc46a19553fb51
Angelo <angelo@Angelo.local>
2018-07-09 06:42:07 +0800

    still no changes

--- partials/footer.php
+++ partials/footer.php
@@ -7,7 +7,7 @@
 	<div class="row border-top border-dark">
     <div class="col-12 text-center " id="footer">
 		<br>
-	<p class="footerBlurb">Designed, Developed, & Deployed by &nbsp;  <a class=" avcvz" href="https://www.avcvz.design/">AVCVZ DESIGN</a>  </p>
+	<p class="footerBlurb">Designed, Developed, & Deployed by &nbsp; <a class="text-dark" id="avcvz" href="https://www.avcvz.design/">avcvz design</a>  </p>
 	</div>
 	<br><br>
 	</div>
